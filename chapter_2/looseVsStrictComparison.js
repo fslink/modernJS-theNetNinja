7c971ad0f71bd89be74de0384b84fe49f08ba789
FS_link <mowglysofiann@gmail.com>
2020-03-13 13:19:54 +0100

    Loose Vs Strict Comparison

---
+++ chapter_2/looseVsStrictComparison.js
@@ -0,0 +1,13 @@
+let age = 25;
+
+//loose comparison
+// console.log(age == 25);
+// console.log(age == '25');
+// console.log(age != 25);
+// console.log(age != '25');
+
+//strict comparison
+console.log(age === 25);
+console.log(age === '25');
+console.log(age !== 25);
+console.log(age !== '25');
