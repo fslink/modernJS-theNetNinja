5883bdb4a43b925e8a3ac1b59aaa795d9b8c4134
FS_link <mowglysofiann@gmail.com>
2020-03-13 13:36:57 +0100

    Type Conversion

---
+++ chapter_2/typeConversion.js
@@ -0,0 +1,10 @@
+let number = '100';
+
+number = Number(number);
+//console.log(number + 3, typeof number);
+
+//let result = Number('hello');
+//let result = String(60);
+let result = Boolean(' ');
+
+console.log(result, typeof result);
