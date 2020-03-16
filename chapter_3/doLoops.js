72a94c0ed29453ee8105f0294decc581451347b0
FS_link <mowglysofiann@gmail.com>
2020-03-17 00:33:33 +0100

    Do While Loops

---
+++ chapter_3/doLoops.js
@@ -0,0 +1,9 @@
+let i = 0;
+
+const names = ['shaun', 'mathieu', 'jedi'];
+
+console.log('taille du tableau : ' + names.length);
+do{
+    console.log('Bonjour ' + names[i]);
+    i++;
+} while(i < names.length);
