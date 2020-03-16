9e67fe932db777b3e2bc020a308a491bd8fb452b
FS_link <mowglysofiann@gmail.com>
2020-03-16 22:40:00 +0100

    While Loops

---
+++ chapter_3/whileLoop.js
@@ -0,0 +1,13 @@
+let i = 0;
+
+// while(i < 5){
+//     console.log('La boucle a eu lieu ' + i);
+//     i++;
+// }
+
+const names = ['shaun', 'mathieu', 'jedi'];
+
+while(i < names.length){
+    console.log('Bonjour ' + names[i]);
+    i++;
+}
