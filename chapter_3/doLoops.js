31e706cc0e027f903bd719f4680bbdb736755368
FS_link <mowglysofiann@gmail.com>
2020-03-18 15:24:32 +0100

    If Statements

--- chapter_3/doLoops.js
+++
@@ -1,9 +0,0 @@
-let i = 0;
-
-const names = ['shaun', 'mathieu', 'jedi'];
-
-console.log('taille du tableau : ' + names.length);
-do{
-    console.log('Bonjour ' + names[i]);
-    i++;
-} while(i < names.length);
