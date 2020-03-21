37d8634b583083a12be94a55ae4fa7c941bcd153
FS_link <mowglysofiann@gmail.com>
2020-03-21 14:35:48 +0100

    changes chapter-4

---
+++ chapter_4/functions.js
@@ -0,0 +1,19 @@
+//function declaration hosting
+
+
+
+//function expression
+const speak = function(){
+    console.log('Expression function !');
+
+}
+
+greet();
+greet();
+greet();
+greet();
+speak();
+
+function greet(){
+    console.log('Declaration function hosting !');
+}
