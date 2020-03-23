9df641ba25c13a1745b6aaa6913e27ac04b9d098
FS_link <mowglysofiann@gmail.com>
2020-03-23 22:26:13 +0100

    Functions Vs Methods

--- chapter_4/arrowFunctions.js
+++
@@ -1,8 +0,0 @@
-// const calcArea = radius => 3.14 * radius**2;
-
-// let area = calcArea(5);
-// console.log(area);
-
-const greet = () => console.log('hello');
-greet();
-
---
+++ chapter_4/methods.js
@@ -0,0 +1,8 @@
+const name = 'shaun';
+const greet = () => 'Salut';
+let resultOne = greet();
+console.log(resultOne);
+
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);
+console.log(name);
