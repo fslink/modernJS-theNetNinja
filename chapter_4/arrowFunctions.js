d19c3e97636c887b705aeb39f49c7ee884194220
FS_link <mowglysofiann@gmail.com>
2020-03-23 15:38:50 +0100

    Arrow Functions

---
+++ chapter_4/arrowFunctions.js
@@ -0,0 +1,8 @@
+// const calcArea = radius => 3.14 * radius**2;
+
+// let area = calcArea(5);
+// console.log(area);
+
+const greet = () => console.log('hello');
+greet();
+
--- chapter_4/returningValues.js
+++
@@ -1,7 +0,0 @@
-const calcArea = function(radius){
-    return 3.14 * radius**2;
-}
-
-let area = calcArea(5);
-console.log(area);
-
