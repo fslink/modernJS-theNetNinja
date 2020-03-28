60875abf542a5a5c31c829f85255cce2546e57ea
FS_link <mowglysofiann@gmail.com>
2020-03-28 15:23:22 +0100

    Math Object

---
+++ chapter_5/math.js
@@ -0,0 +1,12 @@
+console.log(Math);
+console.log(Math.PI);
+
+const area = 7.9;
+
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));
+
+console.log(Math.round(Math.random() * 100));
+console.log((Math.random() * 100));
