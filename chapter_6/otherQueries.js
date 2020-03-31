8ea65bed850350d6d0c206bbe42a1ad3a3f1d346
FS_link <mowglysofiann@gmail.com>
2020-03-31 18:26:47 +0200

    Other Ways to Querie the DOM

---
+++ chapter_6/otherQueries.js
@@ -0,0 +1,11 @@
+// by ID
+const title = document.getElementById('page-title');
+console.log(title);
+
+// by Class
+const errors = document.getElementsByClassName('error');
+console.log(errors);
+
+// by tagname
+const paras = document.getElementsByTagName('p');
+console.log(paras);
