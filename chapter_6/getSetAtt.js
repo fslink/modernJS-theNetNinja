cc04b86120c60c26c3a9c9ce86549b431d1b6dce
FS_link <mowglysofiann@gmail.com>
2020-04-03 15:51:01 +0200

    Changing Css Styles

--- chapter_6/getSetAtt.js
+++
@@ -1,9 +0,0 @@
-const link = document.querySelector('a');
-console.log(link.getAttribute('href'));
-
-link.setAttribute('href', 'http://www.thenetninja.co.uk');
-link.innerText = 'Link to The Net Ninja website !';
-
-const para = document.querySelector('p');
-para.setAttribute('class', 'success');
-para.setAttribute('style', 'color:red;');
