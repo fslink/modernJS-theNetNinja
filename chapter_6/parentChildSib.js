af7608bc6112becdf2563a40319085078c3b9b7e
FS_link <mowglysofiann@gmail.com>
2020-04-04 15:43:07 +0200

    Parents, Children & Siblings

---
+++ chapter_6/parentChildSib.js
@@ -0,0 +1,15 @@
+const article = document.querySelector('article');
+console.log(article.children);
+
+Array.from(article.children).forEach(child => {
+    child.classList.add('article-list');
+});
+
+
+const title = document.querySelector('h2');
+console.log(title.parentElement);
+console.log(title.parentElement.parentElement);
+console.log(title.nextElementSibling);
+console.log(title.previousElementSibling);
+
+console.log(title.nextElementSibling.parentElement.children);
