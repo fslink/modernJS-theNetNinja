1939282e3001118a276cbac615bda94203507ab6
FS_link <mowglysofiann@gmail.com>
2020-04-21 14:20:36 +0200

    Map Method

--- chapter_9/sandbox.js
+++ chapter_9/sandbox.js
@@ -1,18 +1,26 @@
-// const scores = [10, 20, 40, 15, 30, 55];
+// const prices = [50,30,50,80,40,60];
 
-// scoresFiltered = scores.filter((score) => {
-//     return score === 10;
+// const salePrices = prices.map((price) => {
+//     return price / 2;
 // });
 
-// console.log(scoresFiltered);
+// console.log(salePrices);
+// console.log(prices);
 
-const users = [
-    {name: 'shaun', premium: true},
-    {name: 'mario', premium: false},
-    {name: 'luigi', premium: false},
-    {name: 'link', premium: true}
+const products = [
+    {name: 'carapace verte', price: 30},
+    {name: 'carapace bleue', price: 100},
+    {name: 'carapace rouge', price: 50},
+    {name: 'banana skin', price: 10},
 ];
 
-const usersFiltered = users.filter(user => user.premium);
+const saleProducts = products.map((product) => {
+    if(product.price > 30){
+        return {name: product.name, price: product.price / 2};
 
-console.log(usersFiltered);
+    } else {
+        return product;
+    }
+});
+
+console.log(saleProducts);
