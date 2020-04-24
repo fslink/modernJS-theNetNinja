14b9e09cc14b5cfc262ee10e857074eecf19a015
FS_link <mowglysofiann@gmail.com>
2020-04-24 21:29:49 +0200

    Sort Part1

--- chapter_9/find.js
+++
@@ -1,5 +0,0 @@
-const scores = [10,20,5,15,50,90,100,10,58];
-
-const hightScores = scores.find(score => score > 90);
-
-console.log(hightScores);
---
+++ chapter_9/sort.js
@@ -0,0 +1,10 @@
+const scores = [10,20,5,15,50,90,100,10,58];
+const names = ['mario', 'luigi', 'mother-fucker', 'street-fucker', 'midnight-fucker'];
+
+// scores.sort();
+scores.reverse();
+console.log(scores);
+
+// names.sort();
+names.reverse();
+console.log(names);
