fbde43dc53b8e60e3cc1c0b2e8ddba1216696c56
FS_link <mowglysofiann@gmail.com>
2020-04-26 01:52:04 +0200

    Sort Method Part2

--- chapter_9/sort.js
+++ chapter_9/sort.js
@@ -1,10 +1,33 @@
 const scores = [10,20,5,15,50,90,100,10,58];
 const names = ['mario', 'luigi', 'mother-fucker', 'street-fucker', 'midnight-fucker'];
 
-// scores.sort();
+scores.sort((a, b) => b - a);
 scores.reverse();
 console.log(scores);
 
 // names.sort();
-names.reverse();
-console.log(names);
+// names.reverse();
+// console.log(names);
+
+const players = [
+    {name: 'mario', score: 20},
+    {name: 'mother-fucker', score: 200},
+    {name: 'Othelo', score: 15},
+    {name: 'Le Roi', score: 1000},
+    {name: 'luigi', score: 35},
+    {name: 'sysiphe', score: 45}
+];
+
+// players.sort((a, b) => {
+//     if(a.score > b.score){
+//         return -1;
+//     } else if(b.score > a.score){
+//         return 1;
+//     } else {
+//         return 0;
+//     }
+// });
+
+// players.sort((a, b) => b.score - a.score);
+
+// console.log(players);
