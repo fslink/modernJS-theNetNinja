1daf68599b6343a13783686bac16a79f517cf007
FS_link <mowglysofiann@gmail.com>
2020-05-01 18:36:54 +0200

    search todos

--- todo/app.js
+++ todo/app.js
@@ -1,5 +1,6 @@
 const addTodoForm = document.querySelector('.add');
 const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
 
 const generateTemplate = todo => {
 
@@ -30,4 +31,29 @@ list.addEventListener('click', e => {
     if(e.target.classList.contains('delete')){
         e.target.parentElement.remove();
     }
+});
+
+
+const filterTodos = (term) => {
+    Array.from(list.children)
+        .filter((todo) => {
+            return !todo.textContent.toLowerCase().includes(term);
+
+        }).forEach((todo) => {
+            todo.classList.add('filtered');
+        });
+
+        Array.from(list.children)
+        .filter((todo) => {
+            return todo.textContent.toLowerCase().includes(term);
+
+        }).forEach((todo) => {
+            todo.classList.remove('filtered');
+        });
+};
+
+// search todo
+search.addEventListener('keyup', e => {
+    const term = e.target.value.trim().toLowerCase();
+    filterTodos(term);
 });
