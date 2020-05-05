3c72c925080e9e2de6e31210f376991ed9f185b3
FS_link <mowglysofiann@gmail.com>
2020-05-05 17:35:35 +0200

    Making Http Requests(XHR)

--- chapter_11/sandbox.js
+++ chapter_11/sandbox.js
@@ -1,9 +1,14 @@
-console.log(1);
-console.log(2);
+//JSONPLaceHolder
 
-setTimeout(() => {
-    console.log('callback async fired !');
-}, 2000);
+const request = new XMLHttpRequest();
 
-console.log(3);
-console.log(4);
+request.addEventListener('readystatechange', () => {
+    console.log(request, request.readyState);
+    if(request.readyState === 4){
+        console.log(request.responseText);
+    }
+    
+});
+
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+request.send();
