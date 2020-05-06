d16a9cf42e85ab5e6c5c8c8078623e77b03711a4
FS_link <mowglysofiann@gmail.com>
2020-05-06 17:52:02 +0200

    Response Status

--- chapter_11/sandbox.js
+++ chapter_11/sandbox.js
@@ -3,9 +3,11 @@
 const request = new XMLHttpRequest();
 
 request.addEventListener('readystatechange', () => {
-    console.log(request, request.readyState);
-    if(request.readyState === 4){
+    if(request.readyState === 4 && request.status === 200){
         console.log(request.responseText);
+
+    } else if(request.readyState === 4){
+        console.log('we could not fetch data');
     }
     
 });
