6236e41661042d18ae3fb2e900963cde52eac92c
FS_link <mowglysofiann@gmail.com>
2020-05-12 01:46:59 +0200

    The Fetch API

--- chapter_11/sandbox.js
+++ chapter_11/sandbox.js
@@ -1,34 +1,16 @@
 //JSONPLaceHolder
 
-const getTodos = (ressource) => {
-    return new Promise((resolve, reject) => {
-        const request = new XMLHttpRequest();
-
-        request.addEventListener('readystatechange', () => {
-            if(request.readyState === 4 && request.status === 200){
-                const data = JSON.parse(request.responseText);
-                resolve(data);
-
-            } else if(request.readyState === 4){
-                reject('we could not fetch data');
-            }
-            
-        });
-
-        request.open('GET', ressource);
-        request.send();
+fetch('todos/luigi.json').then(response => {
+    return response.json();
+}).then(data => {
+    console.log(data);
+    fetch('todos/soso.json').then(response => {
+        return response.json();
+    }).then(data => {
+        console.log(data);
+    }).catch(err => {
+        console.log(err);
     });
-    
-};
-
-getTodos('todos/mario.json').then(data => {
-    console.log(data);
-    return getTodos('todos/luigi.json');
-}).then(data => {
-    console.log(data);
-    return getTodos('todos/soso.json');
-}).then(data => {
-    console.log(data);
 }).catch(err => {
     console.log(err);
-})
+});
