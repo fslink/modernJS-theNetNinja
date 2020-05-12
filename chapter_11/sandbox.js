41344ba608fd05fb16310bf5533bb20bd6bc56d1
FS_link <mowglysofiann@gmail.com>
2020-05-12 03:01:52 +0200

    Async & Await and Throw Error

--- chapter_11/sandbox.js
+++ chapter_11/sandbox.js
@@ -1,16 +1,32 @@
 //JSONPLaceHolder
 
-fetch('todos/luigi.json').then(response => {
-    return response.json();
-}).then(data => {
+const getTodos = async () => {
+    const response = await fetch('todos/luigi.json');
+
+    if(response.status !== 200){
+        throw new Error('Cannot reach data');
+    }
+
+    const data = await response.json();
+
+    return data;
+}
+
+getTodos().then(data => {
     console.log(data);
-    fetch('todos/soso.json').then(response => {
-        return response.json();
-    }).then(data => {
-        console.log(data);
-    }).catch(err => {
-        console.log(err);
-    });
-}).catch(err => {
-    console.log(err);
-});
+}).catch(err => console.log(err.message));
+
+// fetch('todos/luigi.json').then(response => {
+//     return response.json();
+// }).then(data => {
+//     console.log(data);
+//     fetch('todos/soso.json').then(response => {
+//         return response.json();
+//     }).then(data => {
+//         console.log(data);
+//     }).catch(err => {
+//         console.log(err);
+//     });
+// }).catch(err => {
+//     console.log(err);
+// });
