738270a4a4aa2d044c5f575317c55af83a53c1c2
FS_link <mowglysofiann@gmail.com>
2020-05-19 18:36:41 +0200

    set update & get localStorage

---
+++ chapter_12/app.js
@@ -0,0 +1,11 @@
+// localStorage.setItem('name', 'mario');
+// localStorage.setItem('age', 80);
+
+console.log(localStorage.name, localStorage.age);
+
+const name = localStorage.getItem('name');
+const age = localStorage.getItem('age');
+
+console.log(name, age);
+
+// localStorage.setItem('name', 'luigi');
--- weather-app/scripts/app.js
+++ weather-app/scripts/app.js
@@ -1,13 +1,37 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const cardDts = document.querySelector('.details');
+
+const updateUI = (data) => {
+    // const cityDts = data.cityDts;
+    // const weather = data.weather;
+    // destructure object
+
+    const {cityDts, weather} = data;
+
+    console.log(cityDts, weather);
+
+    cardDts.innerHTML = `
+        <div class="text-muted text-uppercase text-center details">
+            <h5 class="my-3">${cityDts.EnglishName}</h5>
+            <div class="my-3">${weather.WeatherText}</div>
+            <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+            </div>
+        </div>
+    `;
+
+    if (card.classList.contains('d-none')) {
+        card.classList.remove('d-none');
+    }
+};
 
 const updateCity = async (city) => {
     const cityDts = await getCity(city);
     const weather = await getCurrent(cityDts.Key);
 
-    return {
-        cityDts: cityDts,
-        weather: weather
-    };
+    return { cityDts, weather };
 };
 
 cityForm.addEventListener('submit', e => {
@@ -16,6 +40,7 @@ cityForm.addEventListener('submit', e => {
     const city = cityForm.city.value.trim();
     cityForm.reset();
 
-    const cityWeather = updateCity(city);
-    console.log(cityWeather);
+    updateCity(city)
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
 });
