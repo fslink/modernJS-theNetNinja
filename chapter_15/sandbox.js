6fec749cfdca12aabbbbb515a8eca4eb2925d8da
FS_link <mowglysofiann@gmail.com>
2020-05-23 17:58:22 +0200

    Class Method and Method chaining

--- chapter_15/sandbox.js
+++ chapter_15/sandbox.js
@@ -16,10 +16,24 @@ class User {
     constructor(username, email){
         this.username = username;
         this.email = email;
+        this.score = 0;
+    }
+    logIn(){
+        console.log(`${this.username} is logged in`);
+        return this;
+    }
+    logOut(){
+        console.log(`${this.username} is logged out`);
+        return this;
+    }
+    incScore(){
+        this.score += 1;
+        console.log(`The score is ${this.score}`);
+        return this;
     }
 }
 
 const userTwo = new User('mario', 'mario@gmail.com');
 const userThree = new User('luigi', 'luigi@gmail.com');
 
-console.log(userTwo, userThree);
+userTwo.logIn().incScore().incScore().logOut();
