7c17d626ca8d53ef8cd62695f7e6003072999a10
FS_link <mowglysofiann@gmail.com>
2020-05-27 17:12:15 +0200

    Prototypal Inheritance & Built-in Objects

--- chapter_15/sandbox.js
+++ chapter_15/sandbox.js
@@ -12,37 +12,62 @@ const userOne = {
 // console.log(userOne.username, userOne.email);
 // userOne.logIn();
 
-class User {
-    constructor(username, email){
-        this.username = username;
-        this.email = email;
-        this.score = 0;
-    }
-    logIn(){
-        console.log(`${this.username} is logged in`);
-        return this;
-    }
-    logOut(){
-        console.log(`${this.username} is logged out`);
-        return this;
-    }
-    incScore(){
-        this.score += 1;
-        console.log(`The score is ${this.score}`);
-        return this;
-    }
+// class User {
+//     constructor(username, email){
+//         this.username = username;
+//         this.email = email;
+//         this.score = 0;
+//     }
+//     logIn(){
+//         console.log(`${this.username} is logged in`);
+//         return this;
+//     }
+//     logOut(){
+//         console.log(`${this.username} is logged out`);
+//         return this;
+//     }
+//     incScore(){
+//         this.score += 1;
+//         console.log(`The score is ${this.score}`);
+//         return this;
+//     }
+// }
+
+// class Admin extends User{
+//     constructor(username, email, title){
+//         super(username, email);
+//         this.title = title;
+//     }
+//     deleteUSer(user){
+//         users = users.filter(u => u.username !== user.username);
+//     }
+// }
+
+function User(username, email, title){
+    this.username = username;
+    this.email = email;
+    this.title = title;
 }
 
-class Admin extends User{
-    deleteUSer(user){
-        users = users.filter(u => u.username !== user.username);
-    }
+User.prototype.login = function(){
+    console.log(`the user : ${this.username} is logged in !`);
+    return this;
+};
+
+User.prototype.logout = function(){
+    console.log(`the user : ${this.username} is logged out !`);
+    return this;
 }
+
+function Admin(username, email, title){
+    User.call(this, username, email);
+    this.title = title;
+}
+
+Admin.prototype = Object.create(User.prototype);
 
 const userTwo = new User('mario', 'mario@gmail.com');
 const userThree = new User('luigi', 'luigi@gmail.com');
-const userFour = new Admin('sofiann', 'sofiann@gmail.com');
+const userFour = new Admin('sofiann', 'sofiann@gmail.com', 'black-belt-ninja');
 
-let users = [userTwo, userThree, userFour];
-userFour.deleteUSer(userThree);
-console.log(users);
+console.log(userTwo, userThree, userFour);
