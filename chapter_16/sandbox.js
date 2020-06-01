ebc746e8aeb498dc66541281c41ab74bf82b8c05
FS_link <mowglysofiann@gmail.com>
2020-06-01 18:30:11 +0200

    add documents

--- chapter_16/sandbox.js
+++ chapter_16/sandbox.js
@@ -1,11 +1,16 @@
 const recipiesList = document.querySelector('ul');
+const form = document.querySelector('form');
 
-const addRecipie = (recipie) => {
+const addRecipieUI = (recipie) => {
     let title = recipie.data().title;
     let time;
 
     if(recipie.data().date){
         time = recipie.data().date.toDate();
+
+    } else if(recipie.data().created_at) {
+        time = recipie.data().created_at.toDate();
+       
     } else {
         time = 'no date';
     }
@@ -19,8 +24,24 @@ const addRecipie = (recipie) => {
     recipiesList.innerHTML += html;
 };
 
+//get recipies
 db.collection("recipies").get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
-        addRecipie(doc);
+        addRecipieUI(doc);
     });
 });
+
+//add recipies
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const now = new Date();
+
+    const recipe = {
+        title: form.recipe.value,
+        created_at: firebase.firestore.Timestamp.fromDate(now)
+    };
+
+    db.collection("recipies").add(recipe).then(() => {
+        console.log("recipie added");
+    })
+})
