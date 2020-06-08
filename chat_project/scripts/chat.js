435123f5d23a99572ad33cfef15810b21fb9c36b
FS_link <mowglysofiann@gmail.com>
2020-06-08 23:28:01 +0200

    Complex queries, updating username & the room

--- chat_project/scripts/chat.js
+++ chat_project/scripts/chat.js
@@ -5,10 +5,10 @@ class Chatroom {
         this.username = username;
         this.room = room;
         this.chats = db.collection('chats');
+        this.unsub;
     }
     async addChat(message){
         //format message in object
-
         const now = new Date();
         const chat = {
             message: message,
@@ -21,10 +21,46 @@ class Chatroom {
         const response = await this.chats.add(chat);
         return response;
     }
+    getChats(callback){
+        this.unsub = this.chats
+            .where('room', '==', this.room)
+            .orderBy('created_at')
+            .onSnapshot(snapshot => {
+                snapshot.docChanges().forEach(change => {
+                    if(change.type === 'added'){
+                        // callback(change.data());
+                        callback(change.doc.data());
+                    }
+                });
+            });
+    }
+    updateName(username){
+        this.username = username;
+    }
+    updateRoom(room){
+        this.room = room;
+        console.log('room updated');
+        if(this.unsub){
+            this.unsub();
+        }
+    }
 }
 
-const chat = new Chatroom('hello', 'Boris', 'general');
+const chat = new Chatroom('Clovis', 'general');
+chat.getChats((data) => {
+    console.log(data);
+});
 
-chat.addChat('hello eferyone')
-    .then(() => console.log('chat added'))
-    .catch(err => console.log(err));
+setTimeout(() => {
+    chat.addChat('Salut à mes esclaves Gallo-Romains');
+    chat.updateName('Clovis le Catholique');
+    chat.addChat('Salut à mes freres Gallo-Romains');
+    chat.updateRoom('gaming');
+    chat.getChats((data) => {
+        console.log(data);
+    });
+}, 4000)
+
+setTimeout(() => {
+    chat.addChat('Salut à mes amis gamers');
+}, 6000);
