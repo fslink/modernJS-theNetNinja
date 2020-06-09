3aed0e445eec5d102d443fca155fb97ccbafa4ab
FS_link <mowglysofiann@gmail.com>
2020-06-09 18:54:48 +0200

    sending new chats with form

--- chat_project/scripts/app.js
+++ chat_project/scripts/app.js
@@ -1,11 +1,21 @@
 // dom element selection
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+
+// event add new chat
+newChatForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = newChatForm.message.value.trim();
+    chat.addChat(message)
+        .then(() => newChatForm.reset())
+        .catch(err => console.log(err));
+});
 
 // class instances
-const chat = new Chatroom('Clovis', 'gaming');
+const chat = new Chatroom('Clovis', 'general');
 const chatUI = new ChatUI(chatList);
 
-// add chats to UI
+// get chats and render
 chat.getChats((data) => {
     chatUI.render(data);
 });
